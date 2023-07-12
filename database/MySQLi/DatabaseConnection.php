3fbd08b39764c525bae21e92612c840e621ef8a8
Rocio <rocioalonso.dev@gmail.com>
2023-07-12 12:52:23 +0200

    stablished database connection

---
+++ database/MySQLi/DatabaseConnection.php
@@ -0,0 +1,55 @@
+<?php 
+
+namespace Database\MySQLi;
+
+class DatabaseConnection {
+    private $server;
+    private $username;
+    private $password;
+    private $database;
+    private $connection;
+    
+    public function __construct($server, $username, $password, $database){
+        $this -> server = $server;
+        $this -> username = $username;
+        $this -> password = $password;
+        $this -> database = $database;
+    }
+
+    public function connect(){
+        $this -> connection = mysqli_connect($this -> server,$this -> username,$this ->password,$this -> database);
+
+        if ($this -> connection -> connect_errno){
+            die("Falló la conexión: {$this -> connection -> connect_error}");
+        }
+
+        $set_names = $this -> connection -> prepare("SET NAMES 'utf8'");
+        $set_names -> execute();
+    }
+
+    public function execute_query($query,$parameters = []){
+        $statement = $this -> connection -> prepare($query);
+        $results = $statement -> execute($parameters);
+        return $results;
+    }
+    
+
+}
+
+$database = 'gestor-de-tareas';
+$server = 'localhost';
+$username = 'root';
+$password = '';
+
+$db = new DatabaseConnection($server,$username,$password,$database);
+$db -> connect();
+$query = 'SELECT * FROM users';
+$results = $db -> execute_query($query);
+print_r($results);
+
+// foreach($results as $row){
+//     echo $row['username'];
+// }
+
+
+?>
